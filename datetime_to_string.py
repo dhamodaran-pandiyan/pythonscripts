20bdce8a731f54f6826885f224302396fce40748
Dhamodaran Pandiyan <18031856+dhamodaran-pandiyan@users.noreply.github.com>
2019-11-17 15:38:42 +0530

    Delete datetime_to_string.py

--- datetime_to_string.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-datetime_object = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
-print(datetime_object)
-
-print(datetime.strptime('Thu, 16 Dec 2010 12:14:05', '%a, %d %b %Y %H:%M:%S').isoformat())
-print(type(datetime_object))
