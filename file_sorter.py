9c19f1dbf7f60e3cf0cb96cc3fcbb7fb215e11b7
Dhamodaran Pandiyan <18031856+dhamodaran-pandiyan@users.noreply.github.com>
2019-11-17 15:41:01 +0530

    sort files by its extensions by creating folders

---
+++ file_sorter.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+path = 'C:/Users/Dhamodaran Pandiyan/Downloads'
+list_ = os.listdir(path)
+for file_ in list_:
+    name,ext = os.path.splitext(file_)
+    ext = ext[1:]
+    if ext == '':
+        continue
+    if os.path.exists(path+'/'+ext):
+       shutil.move(path+'/'+file_,path+'/'+ext+'/'+file_)
+    else:
+        os.makedirs(path+'/'+ext)
+        shutil.move(path+'/'+file_,path+'/'+ext+'/'+file_)
